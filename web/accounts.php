6c59cd9dd50ac0e027c7126184e484d6e342aeb2
thomas-clayton <34486550+one-jay@users.noreply.github.com>
2020-05-24 09:30:46 -0700

    accounts formatting

--- web/accounts.php
+++ web/accounts.php
@@ -8,28 +8,39 @@ $db = get_db();
 
     </head>
     <body>
-        
-        <?php
+        <h2>Accounts:</h2>
 
+        <table>
+            <thead>
+                <tr>
+                    <th>Account Name</th>
+                    <th>Address</th>
+                    <th>City</th>
+                    <th>State</th>
+                    <th>Zip</th>
+                </tr>
+                
+            </thead>
+            <tbody>
+        <?php
             $statement = $db->prepare("SELECT * FROM account");
             $statement->execute();
 
             while ($row = $statement->fetch(PDO::FETCH_ASSOC))
             {
                 // The variable "row" now holds the complete record for that
-                // row, and we can access the different values based on their
-                // name
+                // row, and we can access the different values based on their name
                 $name = $row['name'];
                 $street = $row['street'];
                 $city = $row['city'];
                 $state = $row['state'];
                 $zip = $row['zip'];
 
-                echo "<h3>Account: $name </h3><br>
-                    <p> Address: $street <br> $city, $state $zip<p>";
+                echo "<tr> <td>$name</td> <td>$street</td> <td>$city</td> <td>$state</td> <td>$zip</td> </tr>";
             }
-
         ?>
+        </tbody>
+        </table>
 
     </body>
 </html>
