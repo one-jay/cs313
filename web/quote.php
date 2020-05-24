e6cfcbc20b2e536bff6cf3f45e58c0d81fe8332b
thomas-clayton <34486550+one-jay@users.noreply.github.com>
2020-05-24 13:20:29 -0700

    quote

---
+++ web/quote.php
@@ -0,0 +1,48 @@
+<?php
+require "crm-db.php";
+$db = get_db();
+$id = $_GET['id'];
+?>
+
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>Quote: <?=$id?> </h1>
+
+        <h2> Quote Lines:</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Quote Line ID</th>
+                    <th>Product</th>
+                    <th>List Price</th>
+                    <th>Quote Price</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php
+            $statement = $db->prepare(" SELECT id, product.name, product.listprice, price, quantity
+                                        FROM quoteline
+                                        JOIN product ON quoteline.product = product.id
+                                        WHERE quote = '".$id."' ");
+            $statement->execute();
+
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+            {
+                $quoteLineId = $row['id'];
+                $product = $row['name'];
+                $listPrice = $row['listprice'];
+                $price = $row['price'];
+                $quantity = $row['quantity'];
+
+                echo "<tr> <td><a href=\"quoteline.php?id=$quoteLineId\">$quoteLineId</a></td> <td>$product</td> <td>$price</td> <td>$quantity</td>  </tr>";
+            }
+        ?>
+        </tbody>
+        </table>
+
+    </body>
+</html>
