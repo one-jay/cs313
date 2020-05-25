def999eb490e511119aaa1accb758eb2a30b9d0b
thomas-clayton <34486550+one-jay@users.noreply.github.com>
2020-05-24 21:33:25 -0700

    nav styling

--- web/crm-db.php
+++ web/crm-db.php
@@ -57,7 +57,7 @@ function get_db() {
 	return $db;
 }
 
-function getContactsAndAccount(){
+function debugQuery(){
     $db = get_db();
     $sql = 'SELECT account.id as acctId, account.name, opportunity.id, stage 
             FROM opportunity
@@ -74,11 +74,46 @@ function getContactsAndAccount(){
 <!doctype html>
 <html>
     <head>
+        <style>
+            body{
+                margin: 0;
 
+            }
+            ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
+                position: fixed;
+                top: 0;
+                width: 100%;
+            }
+
+            li {
+                float: left;
+            }
+
+            li a {
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+            }
+
+            /* Change the link color to #111 (black) on hover */
+            li a:hover {
+                background-color: #111;
+            }
+            .active {
+                background-color: #4CAF50;
+            }
+        </style>
     </head>
     <body>
         <?php
-            print_r (getContactsAndAccount());
+            //print_r (debugQuery());
         ?>
 
         <ul>
