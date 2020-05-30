6ad189f84b4d929d1ca087a0a8bbc0de3e2dd72f
thomas-clayton <34486550+one-jay@users.noreply.github.com>
2020-05-30 10:10:10 -0700

    product

--- web/product.php
+++ web/product.php
@@ -2,6 +2,27 @@
 require "crm-db.php";
 $db = get_db();
 $id = $_GET['id'];
+if ($_POST) {
+
+    // update opportunity
+    if(isset($_POST['updateOpp'])) {
+        try{
+            $db = get_db();
+            $query = 'UPDATE opportunity SET
+                            stage  = :stage
+                        WHERE id = :id';
+            $statement = $db->prepare($query);
+                $statement->bindValue(':id', $id);
+                $statement->bindValue(':stage', $_POST['stage']);
+            $statement->execute();
+        }catch (Exception $ex){
+            echo "Error with DB. Details: $ex";
+            die();
+        }
+    }
+
+    header("Location: " . $_SERVER['REQUEST_URI']);
+    exit();
 ?>
 
 <html>
@@ -11,7 +32,41 @@ $id = $_GET['id'];
     <body>
         <h1>Product: <?=$id?> </h1>
 
-        <h2> Quote Lines:</h2>
+        <h2>Product Details</h2>
+        <table>
+        <?php
+            $statement = $db->prepare(" SELECT * FROM product 
+                                        WHERE id = '".$id."'
+                                        LIMIT 1 ");
+            $statement->execute();
+
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
+                $name = $row['name'];
+                $description = $row['description'];
+                $listprice = $row['listprice'];
+            }
+            $details = array(
+                'Product ID'    => $id,
+                'Product Name'  => $name,
+                'Description'  => $description,
+                'List Price'        => $listprice
+            );
+            foreach($details as $k => $v){
+                echo "<tr><th>$k</th><td>$v</td></tr>";
+            }
+        ?>
+        </table>
+
+        <h2>Update Product</h2>
+        <form action="" method="post">
+            <!-- <input type="text" name="id" value="<?=$id?>"> -->
+            <input type="text" name="name" value="<?=$name?>">
+            <input type="text" name="description" value="<?=$description?>">
+            <input type="text" name="listprice" value="<?=$listprice?>">
+            <input type="submit" name="updateProduct" value="Update Product">
+        </form>
+
+        <h2> Related Quote Lines:</h2>
         <table>
             <thead>
                 <tr>
