3f94a06d0799f1f9c3c19ce42f5d2771b0fcadc0
thomas-clayton <34486550+one-jay@users.noreply.github.com>
2020-06-02 23:20:37 -0700

    resize modal

--- web/opportunity.php
+++ web/opportunity.php
@@ -119,8 +119,8 @@ if ($_POST) {
             <!-- Trigger the modal with a button -->
             <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Quote Builder</button>
             <!-- Modal -->
-            <div class="modal fade modal-lg" id="myModal" role="dialog">
-                <div class="modal-dialog">
+            <div class="modal fade" id="myModal" role="dialog">
+                <div class="modal-dialog  modal-lg">
                     <!-- Modal content-->
                     <div class="modal-content">
                         <div class="modal-header">
