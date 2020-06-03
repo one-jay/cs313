c1c62b08f525355f22f0d4ca6d9de299a10f7f4e
thomas-clayton <34486550+one-jay@users.noreply.github.com>
2020-06-02 22:08:04 -0700

    bootstrap to crm db

--- web/crm-db.php
+++ web/crm-db.php
@@ -72,6 +72,9 @@ function debugQuery(){
 <!doctype html>
 <html>
     <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="crm.css">
         <style>
             
@@ -156,6 +159,9 @@ function debugQuery(){
                     }
                 }
             }
+
         </script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </body>
 </html>
